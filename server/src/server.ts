09c48a45be7cc7e662b0057d9bdaf9682b630b10
Mateus Fugita <mateushiwao@gmail.com>
2023-05-21 20:33:55 -0300

    feat: upload images from request

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: 'uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -16,8 +26,9 @@ app.register(jwt, {
   secret: 'gb4738t4378tyh2190hfr309fy123fh13948',
 })
 
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
